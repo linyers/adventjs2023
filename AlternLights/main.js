71f340e4ce405ba31244faca2276066ba2146ebd
linyers <linyers666@gmail.com>
2023-12-14 01:53:37 -0300

    day 9

--- AlternLights/main.js
+++ AlternLights/main.js
@@ -1,10 +1,13 @@
-function adjustLights(lights) {
-  let first = " "
-  let result = 0
+function adjustLights(lights: string[]): number {
+  const checkLights = (lights: string[]) => {
+    let result = 0
 
-  for (let light of lights) {
-    result += +(light === first)
-    first = light === first ? " " : light
+    for (let i = 1; i < lights.length; i++) {
+      if (lights[i] !== lights[i - 1]) continue;
+      result++
+      lights[i] = ["🟢", "🔴"][+(lights[i] === "🟢")]
+    }
+    return result
   }
-  return result
+  return Math.min(checkLights([...lights]), checkLights([...lights].reverse()))
 }
